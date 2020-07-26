3be4a45708b720cc84c917931f3f0b23a99a895b
David Gonzales <dsg2386@gmail.com>
2020-07-26 22:07:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,5 +4,18 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|
       names.each do |name|
         
-        if !new_hash
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        
+        new_hash[name]key << new_value.to_s
+        
+      end
+    end
+  end
+  new_hash
 end
