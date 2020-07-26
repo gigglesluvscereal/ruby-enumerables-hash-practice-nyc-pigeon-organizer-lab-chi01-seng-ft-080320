c29586865663883889a2d09d14d190e323390018
David Gonzales <dsg2386@gmail.com>
2020-07-26 22:07:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
           new_hash[name][key] = []
         end
         
-        new_hash[name]key << new_value.to_s
+        new_hash[name][key] << new_value.to_s
         
       end
     end
